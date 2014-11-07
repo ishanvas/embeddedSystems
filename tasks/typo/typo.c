c00a418dc3ccae7afde3f375a8c7e3f02c20fcfe
LK <leelakrishnanukala27@gmail.com>
2014-11-07 14:33:20 -0800

    typo changes

--- tasks/typo/typo.c
+++ tasks/typo/typo.c
@@ -1,12 +1,25 @@
-/** @file typo.c
- *
- * @brief Echos characters back with timing data.
- *
- * Links to libc.
- */
+/*Program to accept user input on a prompt
+ *Print the entered input
+ *Print the time taken to type input
+ *Loop back to start
+*/
+
+#include <stdio.h>
+#include <unistd.h>
 
 int main(int argc, char** argv)
 {
-	/* Add code here -- put your names at the top. */
-	return 0;
+	char buffer[20];
+	
+	while(1) {
+		printf("Please enter your prompt below (buffer limit 20)\n");
+		unsigned long time_start = time();
+		int read_count = read (STDIN_FILENO, buffer, 20);
+		unsigned long time_finish = time() - time_start;
+		if(read_count == 20) {
+			printf("\n");
+		}
+		write(STDOUT_FILENO, buffer, read_count);
+		printf("\n%lu\n",time_finish);
+	}
 }
